32845320154ae6ae6fecf04132a6de0da925d8db
KARASU-2000 <yu.ki.corgi.no.865@gmail.com>
2023-12-10 11:33:04 +0900

    [other]close #11 Molecules作成

---
+++ src/components/molecules/mRadioButton.vue
@@ -0,0 +1,30 @@
+<script>
+export default{
+    props:{
+        name: String, //ラジオボタンの名前(同じ名前が1セットのラジオボタン)
+        isFlex: Boolean, //CSSを適用するかどうか
+        itemList: Array["id", "value", "defaultChecked"] //id：HTML上のID、value：ラジオボタンの値、defaultChecked：何かしら受け取るとデフォルトでチェックが入る
+    }
+}
+</script>
+
+<template>
+    <div v-bind:class="{flex: isFlex}">
+        <div v-for="(item, index) in itemList" v-bind:key="index">
+            <input
+                type="radio"
+                v-bind:name="name"
+                v-bind:id="item.id"
+                v-bind:value="item.value"
+                v-bind:checked="item.defaultChecked">
+                {{ item.value }}
+        </div>
+    </div>
+</template>
+
+<style>
+/* 適用すると横並びになる */
+.flex{
+    display: flex;
+}
+</style>
